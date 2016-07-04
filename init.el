0e36b24e7802d0ceb03ad7601aac7f6166095f0f
sswanv <sswanv@gmail.com>
2016-07-05 02:32:43 +0800

    update layer

--- init.el
+++ init.el
@@ -37,6 +37,7 @@ values."
      syntax-checking
      sswanv
      lua
+     c-c++
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
--- sswanv/packages.el
+++ sswanv/packages.el
@@ -1,38 +1,8 @@
-;;; packages.el --- sswanv layer packages file for Spacemacs.
-;;
-;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
-;;
-;; Author: John <Rawlins@sswanv.local>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; License: GPLv3
-
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `sswanv-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `sswanv/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `sswanv/pre-init-PACKAGE' and/or
-;;   `sswanv/post-init-PACKAGE' to customize the package as it is loaded.
-
-;;; Code:
-
 (defconst sswanv-packages
   '(youdao-dictionary
     company
-    lua-mode))
+    lua-mode
+    protobuf-mode))
 
 (defun sswanv/init-youdao-dictionary ()
   (use-package youdao-dictionary
@@ -49,4 +19,8 @@
 (defun sswanv/post-init-lua-mode ()
   (setq lua-indent-level 4))
 
-;;; packages.el ends here
+(defun sswanv/init-protobuf-mode ()
+  (use-package protobuf-mode
+    :defer t
+    :mode ("\\.proto\\'" . protobuf-mode)))
+
